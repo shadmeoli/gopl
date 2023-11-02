2208bcf6d224c56b29d36b71a7da7edfca13a611
shadmeoli <shadrack@shopzetu.com>
2023-11-02 16:15:47 +0300

    feat: library installation setup

--- gopl.go
+++ gopl.go
@@ -10,6 +10,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 )
 
 // main CLI entry
@@ -40,6 +41,17 @@ func main() {
 	}
 }
 
+// InstallLibrary installs a Go library and returns an error if the installation fails.
+func InstallLibrary(library string) error {
+	cmd := exec.Command("go", "get", "-u", library)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("Error installing %s: %v", library, err)
+	}
+	return nil
+}
+
 // creator function
 func createProjectStructure(projectName string, useDocker bool) {
 
@@ -47,12 +59,10 @@ func createProjectStructure(projectName string, useDocker bool) {
 	if _, err := os.Stat(projectName); !os.IsNotExist(err) {
 		log.Fatalf("Directory %s already exists. Please choose a different project name.", projectName)
 	}
-
 	// Create the project directory and its parent directories
 	if err := os.MkdirAll(projectName, os.ModePerm); err != nil {
 		log.Fatalf("Failed to create project directory: %v", err)
 	}
-
 	// Change to the project directory
 	if err := os.Chdir(projectName); err != nil {
 		log.Fatalf("Failed to change to the project directory: %v", err)
@@ -60,7 +70,7 @@ func createProjectStructure(projectName string, useDocker bool) {
 
 	// Defining the directory and file structure
 	directories := []string{
-		"cmd/myapi",
+		"cmd",
 		"internal/api/handlers",
 		"internal/app/config",
 		"internal/app/database/postgres",
@@ -69,13 +79,14 @@ func createProjectStructure(projectName string, useDocker bool) {
 		"scripts",
 		"web",
 	}
+
 	// file structure definition
 	files := []string{
 		"Dockerfile",
 		"go.mod",
 		"go.sum",
 		"README.md",
-		"cmd/myapi/main.go",
+		"cmd/main.go",
 		"internal/api/api.go",
 		"internal/app/app.go",
 		"internal/app/database/postgres/postgres.go",
@@ -102,6 +113,47 @@ func createProjectStructure(projectName string, useDocker bool) {
 	}
 	filesProgressBar.Finish()
 
+	// installing needed dependencies for an API -  libraries of choice [ go-fiber, goorm, jwt-go, postgres ]
+	// [
+	// go get -u github.com/gofiber/fiber/v2
+	// go get -u gorm.io/gorm
+	// go get -u gorm.io/driver/postgres
+	// go get -u github.com/dgrijalva/jwt-go
+	// ]
+
+	// List of library import paths to install
+	librariesToInstall := []string{
+		"github.com/gofiber/fiber/v2",
+		"gorm.io/gorm",
+		"gorm.io/driver/postgres",
+		"github.com/dgrijalva/jwt-go",
+	}
+
+	// Create a progress bar
+	libsInstallProgressBar := pb.StartNew(100)
+
+	// Use a WaitGroup to wait for all installations to complete
+	var wg sync.WaitGroup
+
+	for _, library := range librariesToInstall {
+		wg.Add(1)
+		go func(lib string) {
+			defer wg.Done()
+			if err := InstallLibrary(lib); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("Installed %s\n", lib)
+			}
+			libsInstallProgressBar.Increment()
+		}(library)
+	} // Wait for all installations to complete
+	wg.Wait()
+
+	// Close the progress bar
+	libsInstallProgressBar.Finish()
+
+	fmt.Println("All installations complete")
+
 	// Prompting the user to create a .env file
 	fmt.Print("Do you want to create a .env file? (Y/n): ")
 	var createEnvFile string
@@ -109,7 +161,7 @@ func createProjectStructure(projectName string, useDocker bool) {
 
 	if strings.ToLower(createEnvFile) == "y" {
 		// Create a .env file
-		envFilePath := filepath.Join(projectName, ".env")
+		envFilePath := filepath.Join(".env")
 		envFileContent := `
 # Environment Configuration
 DATABASE_URL=your_database_url
@@ -132,8 +184,9 @@ RUN go build -o main cmd/myapi/main.go
 EXPOSE 8080
 CMD ["./main"]
 `
-		dockerfilePath := filepath.Join(projectName, "Dockerfile")
+		dockerfilePath := filepath.Join("Dockerfile")
 		if err := os.WriteFile(dockerfilePath, []byte(dockerfileContent), os.ModePerm); err != nil {
+
 			log.Fatalf("Failed to create Dockerfile: %v", err)
 		}
 
